79f9ae00261b8927d12214f1b8b2b547d059b8e9
nik frank <nikbfrank@gmail.com>
2019-08-19 15:01:02 -0700

    dir

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 exports.handler = (event, context)=>{
-  context.done(null, { statusCode: 200 });
+  
+  context.done(null, {
+    "statusCode": 200,// |301|4XX|5XX,
+    "headers":{
+      "Content-Type": "text/html",
+    },
+    "body": require('fs').readFileSync('./build'+event.path).toString()
+  });
 }
