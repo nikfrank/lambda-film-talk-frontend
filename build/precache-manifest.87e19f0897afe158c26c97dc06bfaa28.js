3d5ffd656e3400671a34e1da85889e6ea34e2bd7
nik frank <nikbfrank@gmail.com>
2019-08-19 14:47:18 -0700

    build

---
+++ build/precache-manifest.87e19f0897afe158c26c97dc06bfaa28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d7192936dfa274c99ffcf2e672909d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dccf98f758be6489129",
+    "url": "/static/css/main.911fa733.chunk.css"
+  },
+  {
+    "revision": "96f91563c4a8de2f3d23",
+    "url": "/static/js/2.6b73252b.chunk.js"
+  },
+  {
+    "revision": "4dccf98f758be6489129",
+    "url": "/static/js/main.576aadce.chunk.js"
+  },
+  {
+    "revision": "761e514753e358c524b4",
+    "url": "/static/js/runtime~main.cf2cc8f3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { loggedIn: false, password: '' }
+
+  setPassword = e => this.setState({ password: e.target.value })
+
+  login = ()=> fetch('/login', {
+    method: 'POST', headers: { 'Content-Type': 'application/json' }, mode: 'cors',
+    body: JSON.stringify({ password: this.state.password})
+  })
+    .then(response => response.statusCode > 400 ? console.error(401) : this.setState({ loggedIn: true }))
+    
+  render() {
+    const { loggedIn, password } = this.state;
+    
+    return (
+      <div className="App">
+        { loggedIn ? (
+            <img src='https://3k92h7oq73.execute-api.us-west-2.amazonaws.com/test/static?key=five-out2.png' />
+        ) : (
+            <div>
+              <input onChange={this.setPassword} type='password' value={password}/>
+              <button onClick={this.login}>LOGIN</button>
+            </div>
+        ) }
+      </div>
+    );
+  }
 }
 
 export default App;
