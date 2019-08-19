74454dbc3fbf0be4a6946d3ba7a2cb6b2a3e7367
nik frank <nikbfrank@gmail.com>
2019-08-19 15:09:28 -0700

    fix

---
+++ build/precache-manifest.a2434c715539f238634b796f5d4fac6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "678ff521f957f3ece48fe654e0ee67a9",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "7c37f1b1301789ead04c",
+    "url": "/test/static/css/main.911fa733.chunk.css"
+  },
+  {
+    "revision": "96f91563c4a8de2f3d23",
+    "url": "/test/static/js/2.6b73252b.chunk.js"
+  },
+  {
+    "revision": "7c37f1b1301789ead04c",
+    "url": "/test/static/js/main.3d0f4549.chunk.js"
+  },
+  {
+    "revision": "2a7197329680a45a62e7",
+    "url": "/test/static/js/runtime~main.15c12310.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test/static/media/logo.5d5d9eef.svg"
+  }
+]);
